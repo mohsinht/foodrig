2ac5779c0f59024ecae6e017c754a98a8985a442
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-02-16 09:16:27 +0500

    as

--- src/main/HomePage.js
+++ src/main/HomePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Input } from 'antd';
+const { Search } = Input;
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -6,12 +8,30 @@ class HomePage extends React.Component {
   }
   render() {
     console.log();
+    const profile = this.props.profileData;
+    const dash_content = profile.customer_profile ? (
+      <div className='new-container'>
+        <div className='new-title'>Add a new Order</div>
+
+        <div className='new-search'>
+          <Search
+            placeholder='search nearby kitchens'
+            onSearch={value => console.log(value)}
+            style={{ maxWidth: 400 }}
+          />
+        </div>
+      </div>
+    ) : (
+      ''
+    );
+
     const user_first_name = this.props.profileData
       ? this.props.profileData.first_name
       : '';
     return (
       <div>
         Welcome <strong>{user_first_name}</strong>
+        {dash_content}
       </div>
     );
   }
